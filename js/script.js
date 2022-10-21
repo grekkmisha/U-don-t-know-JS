f2039d53ae1cf61090726818b35374ccf5df5231
Misha Grekk <grekkmisha@gmail.com>
2022-10-21 16:23:22 +0300

    Исправил 10 урок

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 'use strict';
 
-const adv = document.querySelectorAll('.adv'),
+const adv = document.querySelector('.adv'),
     body = document.querySelector('body'),
     book = document.querySelectorAll('.book'),
-    title = document.querySelectorAll('a'),
-    ul = document.querySelectorAll('ul'),
-    li = document.querySelectorAll('li'),
-    liTag = document.createElement('li');
+    bookNumber = document.querySelectorAll('a'),
+    ulOfTheSecondBook = document.querySelector('ul'),
+    liOfTheSecondBook = ulOfTheSecondBook.querySelectorAll('li'),
+    ulOfTheFifthBook = document.querySelectorAll('ul')[5],
+    liOfTheFifthBook = ulOfTheFifthBook.querySelectorAll('li'),
+    liTag = document.createElement('li'),
+    ulOfTheSixthBook = document.querySelectorAll('ul')[2],
+    liOfTheSixthBook = ulOfTheSixthBook.querySelectorAll('li');
 
-adv[0].remove();
+adv.remove();
 
 body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 
@@ -16,26 +20,15 @@ book[1].after(book[0]);
 book[4].after(book[3]);
 book[5].after(book[2]);
 
-title[4].innerHTML = 'Книга 3. this и <strong>Прототипы</strong> Объектов';
+bookNumber[4].innerHTML = 'Книга 3. this и <strong>Прототипы</strong> Объектов';
 
-ul[0].append(li[7]);
-ul[0].append(li[9]);
-ul[0].append(li[2]);
-ul[0].append(li[10]);
+liOfTheSecondBook[3].after(liOfTheSecondBook[6]);
+liOfTheSecondBook[6].after(liOfTheSecondBook[8]);
+liOfTheSecondBook[9].after(liOfTheSecondBook[2]);
 
-ul[0].prepend(li[5]);
-ul[0].prepend(li[4]);
-ul[0].prepend(li[8]);
-ul[0].prepend(li[6]);
-ul[0].prepend(li[3]);
-ul[0].prepend(li[1]);
-ul[0].prepend(li[0]);
-
-li[47].after(li[55]);
-li[55].after(li[49]);
-li[49].after(li[50]);
-li[48].after(li[52]);
-li[52].after(li[53]);
+liOfTheFifthBook[1].after(liOfTheFifthBook[9]);
+liOfTheFifthBook[4].after(liOfTheFifthBook[2]);
+liOfTheFifthBook[7].after(liOfTheFifthBook[5]);
 
 liTag.textContent = ('Глава 8: За пределами ES6');
-li[25].after(liTag);
+liOfTheSixthBook[8].after(liTag);
